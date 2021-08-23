d3c4463164525d974c4656454f31701e4339a6b9
Patrick Surry <patrick@hopper.com>
2021-08-23 09:17:02 -0400

    updates xplane mapping

--- xplane/fetch.py
+++ xplane/fetch.py
@@ -1,27 +1,33 @@
-# https://developer.x-plane.com/datarefs/
+from . import xpc  # Copied from https://github.com/nasa/XPlaneConnect/blob/master/Python3/src/xpc.py
 
-from . import xpc
+from ruamel.yaml import YAML
 
 import json
 from os import path
 
-drefs = json.load(open(path.join(path.dirname(__file__), 'mapping.json')))
+
+yaml = YAML(typ='safe')
+with open(path.join(path.dirname(__file__), 'mapping.yml')) as f:
+    mapping = yaml.load(f)['metrics']
 
 
 def pollMetrics():
+    drefs = [m['dref'] for m in mapping]
     with xpc.XPlaneConnect() as client:
-        vecs = client.getDREFs(drefs.keys())
+        vecs = client.getDREFs(drefs)
+    data = dict(zip(drefs, vecs))
     metrics = {}
-    for (vec, ms) in zip(vecs, drefs.values()):
-        metrics.update(zip(ms if isinstance(ms, list) else [ms], vec))
-
-    #TODO these conversions should be somewhere else
-    if 'pressureSetting' in metrics:
-        metrics['pressureSetting'] *= 33.8639  # inches of mercury => hectopascals
-    if 'toFrVOR' in metrics:
-        # 0 is flag, 1 is to, 2 is from.
-        metrics['reliabilityVOR'] = metrics['toFrVOR'] == 0
-        metrics['toFrVOR'] -= 1  # to=0, from=1
+    for (m, vec) in zip(mapping, vecs):
+        k = m['metric']
+        ks = k if isinstance(k, list) else [k]
+        if 'fx' in m:
+            vec = [eval(m['fx'], None, dict(x=v, data=data)) for v in vec]
+        kvs = zip(ks, vec)
+        if m.get('unit'):
+            d = {k: dict(value=v, unit=m['unit']) for (k, v) in kvs}
+        else:
+            d = dict(kvs)
+        metrics.update(d)
     return metrics
 
 
